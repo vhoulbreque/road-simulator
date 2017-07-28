0fddf83c5790c5a51f186751dd541f604a6e902b
Houlbreque <vincenthoulbreque@gmail.com>
2017-07-28 16:19:56 -0700

    change in colors

--- my_simulator_hangar.py
+++ my_simulator_hangar.py
@@ -8,8 +8,6 @@ from simulator import Simulator
 
 if __name__ == '__main__':
 
-    # shadow_colors = shadow_colors_init()
-    shadow_colors = [(0, 0, 0)]
 
     xy0_range = [[x, 200] for x in range(0, 250+1)]  # add bottom edge of the picture
     xy1_range = [[0, y] for y in range(int(200/2), 0, -1)]  # add left edge of the picture
@@ -18,13 +16,12 @@ if __name__ == '__main__':
     radius_range = list(range(200, 500)) + list(range(5000, 5300))
     thickness_range = [6, 7, 8, 9, 10]
 
-    color_range = [(255, 0, 125)]
-    white_range = [(255, 255, 255)]
-    yellow_range = [(0, 0, 255)]
-    # color_range = color_range_init()
-    #
-    # white_range = white_range_init()
-    # yellow_range = yellow_range_init()
+    color_range = color_range_init()
+
+    white_range = white_range_init()
+    yellow_range = yellow_range_init()
+    shadow_colors = shadow_colors_init()
+
 
     background_layer = Background(n_backgrounds=100, path='gpics', n_rot=1, n_res=1, n_crop=1, input_size=(250, 200))
     lines_layer = DrawLines(xy0_range, xy1_range, radius_range, thickness_range, color_range, white_range, yellow_range)
