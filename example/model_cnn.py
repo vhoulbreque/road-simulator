40965a9720e1582d2f1bb2d236384ce020082a06
Houlbreque <vincenthoulbreque@gmail.com>
2017-08-03 10:01:55 -0700

    script of creation of keras model added

---
+++ example/model_cnn.py
@@ -0,0 +1,86 @@
+import keras
+from keras import callbacks
+from keras.layers import Input, Convolution2D, MaxPooling2D, Activation
+from keras.layers import Dropout, Flatten, Dense, BatchNormalization
+from keras.models import Model
+from keras.optimizers import Adam
+
+import sys
+sys.path.insert(0, '../src/')
+
+from models.utils import get_datasets
+
+
+paths = ['sample_simple', 'sample_dashed', 'my_dataset']
+n_dataset = 100 * 1000  # number of images
+model_path = 'my_autopilot.hdf5'
+
+# Get train, test and val sets
+train_X, train_Y, val_X, val_Y, test_X, test_Y = get_datasets(paths, limit=n_dataset)
+
+print('train_X[0]: ', train_X[0])
+print('train_Y[0]: ', train_Y[0])
+
+# Create the model
+
+dropout_value = 0.1
+input_shape = (70, 250, 3)
+
+img_in = Input(shape=input_shape, name='img_in')
+x = img_in
+
+x = Convolution2D(1, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
+x = Dropout(dropout_value)(x)
+x = BatchNormalization()(x)
+
+x = Convolution2D(2, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
+x = Dropout(dropout_value)(x)
+x = BatchNormalization()(x)
+
+x = Convolution2D(2, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
+x = Dropout(dropout_value)(x)
+x = BatchNormalization()(x)
+
+x = Convolution2D(4, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D(pool_size=(2, 2), strides=(2,2))(x)
+x = Dropout(dropout_value)(x)
+x = BatchNormalization()(x)
+
+flat = Flatten()(x)
+
+x = Dense(20)(flat)
+x = Activation('relu')(x)
+
+x = Dense(5)(x)
+angle_out = Activation('softmax')(x)
+
+adam = Adam()
+model = Model(input=[img_in], output=[angle_out])
+model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
+
+print(model.summary())
+
+#Save the model after each epoch if the validation loss improved.
+save_best = callbacks.ModelCheckpoint(model_path,
+                                        monitor='val_loss',
+                                        verbose=1,
+                                        save_best_only=True,
+                                        mode='min')
+#stop training if the validation loss doesn't improve for 5 consecutive epochs.
+early_stop = callbacks.EarlyStopping(monitor='val_loss',
+                                        min_delta=0,
+                                        patience=2,
+                                        verbose=0,
+                                        mode='auto')
+callbacks_list = [save_best, early_stop]
+
+
+model.fit([train_X], train_Y,
+            batch_size=100,
+            nb_epoch=20,
+            validation_data=([val_X], val_Y),
+            callbacks=callbacks_list,
+            verbose=2)
---
+++ src/models/utils.py
@@ -0,0 +1,120 @@
+import os
+import numpy as np
+
+from tqdm import tqdm
+from scipy.misc import imread
+from random import shuffle
+
+
+def get_images(paths, limit=1000):
+
+    images = []
+    labels = []
+
+    n = 0
+    for path in paths:
+        if n > limit: break
+        print(path)
+        for image_file in tqdm(os.listdir(path)):
+            if n > limit: break
+            if '.jpg' not in image_file: continue
+            try:
+                img = imread(os.path.join(path, image_file))
+                itc = image_file[:-4].split('_')
+                lbl = [float(itc[3]), float(itc[5])]
+                if img is not None:
+                    images.append(img[:, :])
+                    labels.append(lbl)
+                    n += 1
+            except Exception as e:
+                pass
+
+    images = np.array(images)
+    labels = np.array(labels)
+
+    return images, labels
+
+
+def shuffle_data(X, Y):
+    assert len(X) == len(Y)
+    p = np.random.permutation(len(X))
+    return X[p], Y[p]
+
+
+def from_continue_to_discrete(Y):
+
+    def one_hot(i, n):
+        t = [0 for j in range(n)]
+        t[i] = 1
+        return t
+
+    # transform direction angle into a 5 dimensions array of 0 and 1
+    arr_discr = [i for i in range(-2, 3)]
+    threshs = [-100000, -0.7, -0.25, 0.25, 0.7, 100000]
+    ns = [0 for i in range(-2, 3)]
+
+    Y_new = []
+    for elt in Y:
+        e = elt[1]
+        for i in range(len(threshs)):
+            if threshs[i] < e < threshs[i+1]:
+                t = one_hot(i, 5)
+                Y_new.append(t)
+                ns[i] += 1
+                break
+
+    Y = np.array(Y_new)
+    return Y, ns
+
+
+def equilibrate_dataset(X, Y, ns):
+    m = min(ns)
+
+    X_by_label = [[] for i in ns]
+    Y_by_label = [[] for i in ns]
+
+    for i in range(len(Y)):
+        index = np.argmax(Y[i])
+        Y_by_label[index].append(Y[i])
+        X_by_label[index].append(X[i])
+
+    all_X = []
+    all_Y = []
+
+    for i in range(len(ns)):
+        x, y = np.array(X_by_label[i]), np.array(Y_by_label[i])
+        x, y = shuffle_data(x, y)
+        x = x[:m]
+        y = y[:m]
+        all_X.extend(x)
+        all_Y.extend(y)
+
+    return np.array(all_X), np.array(all_Y)
+
+
+def get_datasets(paths, limit):
+
+    X, Y = get_images(paths, limit=limit)
+
+    # A classifier is better (when talking about performance) than a simple
+    # sigmoid
+    Y, ns = from_continue_to_discrete(Y)
+
+    # Equilibrate the dataset between all the possible directions
+    X, Y = equilibrate_dataset(X, Y, ns)
+
+    # Shuffle everything
+    X, Y = shuffle_data(X, Y)
+
+    # Normalization of the input data (between 0 and 1)
+    X = X.astype('float32') / 255.
+
+    # Split between train, val and test
+    test_cutoff = int(len(X) * .8) # 80% of data used for training
+    val_cutoff = test_cutoff + int(len(X) * .1) # 10% of data used for validation and 10% for test data
+
+    train_X, train_Y = X[:test_cutoff], Y[:test_cutoff]
+    val_X, val_Y = X[test_cutoff:val_cutoff], Y[test_cutoff:val_cutoff]
+    test_X, test_Y = X[val_cutoff:], Y[val_cutoff:]
+
+    return train_X, train_Y, val_X, val_Y, test_X, test_Y
